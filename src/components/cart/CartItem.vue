32d17dcc154f322263ab5b482fde98e6dff02722
costa-beatriz <costa.beatriz.batata@gmail.com>
2026-06-16 21:09:04 -0300

    atualizacao

---
+++ src/components/cart/CartItem.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  item: Object
+})
+
+const emit = defineEmits([
+  'increase',
+  'decrease',
+  'remove'
+])
+</script>
+
+<template>
+
+  <div>
+
+    <h4>{{ item.title }}</h4>
+
+    <button @click="emit('decrease', item.id)">
+      -
+    </button>
+
+    {{ item.quantity }}
+
+    <button @click="emit('increase', item.id)">
+      +
+    </button>
+
+    <button @click="emit('remove', item.id)">
+      Remover
+    </button>
+
+  </div>
+
+</template>
---
+++ src/components/cart/CartPanel.vue
@@ -0,0 +1,51 @@
+<script setup>
+import CartItem from './CartItem.vue'
+import CartSummary from './CartSummary.vue'
+
+defineProps({
+  cart: Array,
+  total: Number
+})
+
+const emit = defineEmits([
+  'increase',
+  'decrease',
+  'remove'
+])
+</script>
+
+<template>
+
+  <div class="cart">
+
+    <h2>Carrinho</h2>
+
+    <div v-if="cart.length === 0">
+      Seu carrinho está vazio.
+    </div>
+
+    <div v-else>
+
+      <CartItem
+        v-for="item in cart"
+        :key="item.id"
+        :item="item"
+        @increase="emit('increase', $event)"
+        @decrease="emit('decrease', $event)"
+        @remove="emit('remove', $event)"
+      />
+
+      <CartSummary :total="total" />
+
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+.cart{
+  border-left:1px solid #ccc;
+  padding:20px;
+}
+</style>
