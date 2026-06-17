98155890925f183b188a89f628ea638da5ef8e4e
costa-beatriz <costa.beatriz.batata@gmail.com>
2026-06-16 21:39:12 -0300

    atualizacao2.0

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  product: Object
+})
+
+const emit = defineEmits(['add'])
+</script>
+
+<template>
+  <div class="card">
+
+    <img :src="product.image">
+
+    <h3>{{ product.title }}</h3>
+
+    <p>{{ product.author }}</p>
+
+    <p>R$ {{ product.price }}</p>
+
+    <button @click="emit('add', product)">
+      Adicionar
+    </button>
+
+  </div>
+</template>
+
+<style scoped>
+.card{
+  border:1px solid #ccc;
+  padding:15px;
+  border-radius:10px;
+}
+</style>
